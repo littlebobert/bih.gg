cd06aa582b877eaa3b15577d2471adb968b553bc
Justin Garcia <justin.garcia@gmail.com>
2025-07-03 16:09:08 -0400

    saving work

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+require "bundler/setup"
+require "openai"
+
+class ApplicationController < ActionController::Base
+	def home
+		render 'home'
+	end
+	
+	def answer
+		query = params[:q]
+		
+		openai = OpenAI::Client.new(
+		  api_key: ENV["OPENAI_API_KEY"]
+		)
+		
+		@completion = openai.chat.completions.create(
+		  messages: [{role: "user", content: query}],
+		  model: :"o3-mini"
+		)
+		
+		puts @completion
+		
+		if @completion && @completion.choices && @completion.choices[0] && @completion.choices[0].message && @completion.choices[0].message.content
+			@answer = @completion.choices[0].message.content
+		end
+		
+		render 'answer'
+	end
+end
